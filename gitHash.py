c70ef16426d1aa9a3962a63a3813e39b0e43f39e
cphenicie <phenicie@princeton.edu>
2017-01-02 15:50:57 -0600

    Changed gitHash.py a bit

--- gitHash.py
+++ gitHash.py
@@ -1,4 +1,13 @@
 # Minimal working example of code that grabs the git hash for a file
+#
+# Other notes about github:
+# To make a command line argument M-!
+# First create the repo then clone with "git clone <url>
+# When committing, make sure the use "double quotes"
+# I think I only made pushing work from the github GUI?
+# To complile from withing emacs, M-! python <file name> RET
+
+
 import subprocess
-hashID = subprocess.check_output(["git", "describe"])
-print(hashID)
+hashID = subprocess.check_output(["git", "describe", "--always"])
+print(hashID.strip())
