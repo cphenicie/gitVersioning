c95ce3732602d2721bf659b3dc854b9c3d25f576
cphenicie <phenicie@princeton.edu>
2017-01-02 16:17:53 -0600

    First version to diff with HEAD

--- gitHash.py
+++ gitHash.py
@@ -11,3 +11,9 @@
 import subprocess
 hashID = subprocess.check_output(["git", "describe", "--always"])
 print(hashID.strip())
+
+isDifferent = subprocess.check_output(["git", "diff", "HEAD"])
+print(isDifferent)
+
+# The first time this was pushed it had the id 3ca5939
+# The second time it had the id c70ef16
