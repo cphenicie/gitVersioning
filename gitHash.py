fa9900272c4497f34c822771a45da1eb4ea355e1
cphenicie <phenicie@princeton.edu>
2017-01-03 23:38:19 -0600

    2017-01-03 23:38:19

--- gitHash.py
+++ gitHash.py
@@ -1,9 +1,5 @@
-# Minimal working example of code that grabs the git hash for a file
-#
-# TODO:
-# 1. Make a tlab_git module
-# 2. Checkout gitpython
-#
+# Example using the tlab_git module
+##
 # Other notes about github:
 # To make a command line argument M-!
 # First create the repo then clone with "git clone <url>
@@ -14,10 +10,7 @@
 # To restore previous versions in emacs, click on the Git:master button or
 # C-x v ~
 
-import subprocess
 import os
-import time
-import git
 
 thisFile = os.path.basename(__file__)
 thisFileDir = os.path.dirname(os.path.realpath(__file__))
@@ -25,43 +18,8 @@ thisFileDir = os.path.dirname(os.path.realpath(__file__))
 os.sys.path.append(thisFileDir + "\..")
 import tlab_git
 
-hashID = subprocess.check_output(["git", "describe", "--always"])
-os.chdir('../..')
-# Make sure this works even if the system is in a different directory
-# print(os.system('pwd'))
-hashID2 = subprocess.check_output(
-    ["git", "rev-list", "-1", "HEAD", "./"], cwd=thisFileDir)
-hashID3 = tlab_git.getHash(gitDir=thisFileDir)
-
-print(hashID.strip())
-print(hashID2.strip()[0:7])
-print(hashID3[0:7])
-
-os.chdir("GitTest\gitVersioning")
-# print(os.system('pwd'))
-
-#diffStr = subprocess.check_output(["git", "diff", "HEAD"])
-# if there is a difference between the current file and "HEAD", it will
-# have this line
-#diffLine = "diff --git a/" + thisFile + " b/" + thisFile
-# .find() returns 0 if found, -1 if not found
-#isDiff = diffStr.find(diffLine) + 1
+hashStr = tlab_git.getHash(gitDir=thisFileDir)
+print(hashStr[0:7])
 isDiff = tlab_git.checkDiff(thisFileDir, thisFile)
-
 if isDiff:
-    # commit to git and grab new hash
-    # print("Pushing changes to git...")
-    # subprocess.check_output(["git", "add", thisFile])
-    # timestr = time.strftime("%Y-%m-%d %H:%M:%S")
-    # subprocess.check_output(["git", "commit", "-m", timestr])
-    # # subprocess.check_output(["git", "push", "origin", "master"]) # This line
-    # # breaks everything
-    # # newHashID = subprocess.check_output(["git", "describe", "--always"])
-    # newHashID = subprocess.check_output(
-    #     ["git", "rev-list", "-1", "HEAD", "./"], cwd=thisFileDir)
-    # print("The new hash is " + newHashID)
     tlab_git.updateRepo(thisFileDir, thisFile)
-
-# Random line changed h
-# The first time this was pushed it had the id 3ca5939
-# The second time it had the id c70ef16
